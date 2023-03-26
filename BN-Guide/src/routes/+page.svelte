dcc888d427ce21c083817263cd768b322402e824
LucioFurnari <luciofurnari99@gmail.com>
2023-03-26 17:30:59 -0300

    Remove Fetch page

--- BN-Guide/src/routes/+page.svelte
+++ BN-Guide/src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-  /** @type {import('./$types').PageData} */
-  export let data
   import Fetch from "./Fetch.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Fetch {data}/>
+<h1>Welcome to BN GUIDE</h1>
+
+<a href="./catalog">Catalog</a>
