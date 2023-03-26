ddca2d2f22fad1ad78a2f4777b2154f3d12afbc3
LucioFurnari <luciofurnari99@gmail.com>
2023-03-25 21:29:00 -0300

    Create data from the load function to use as props

--- BN-Guide/src/routes/+page.svelte
+++ BN-Guide/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data
+  import Fetch from "./Fetch.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Fetch {data}/>
