6f0c8c7783bcd47660eae3207391c2e9dc5733f9
LucioFurnari <luciofurnari99@gmail.com>
2023-03-26 20:33:10 -0300

    Add global style

--- BN-Guide/src/routes/+page.svelte
+++ BN-Guide/src/routes/+page.svelte
@@ -3,3 +3,18 @@
 <h1>Welcome to BN GUIDE</h1>
 
 <a href="./catalog">Catalog</a>
+
+<style>
+  :global(*) {
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+  }
+  :global(a) {
+    color: rgb(240,240,240);
+  }
+  :global(body) {
+    background-color: rgb(25,25,25);
+    color: rgb(240,240,240);
+  }
+</style>
