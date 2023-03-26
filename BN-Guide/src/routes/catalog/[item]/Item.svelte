364bce1a63a3c68758ca913b541e171fa87d08e0
LucioFurnari <luciofurnari99@gmail.com>
2023-03-26 20:17:00 -0300

    Create Item svelte component to use in the routes pages

---
+++ BN-Guide/src/routes/catalog/[item]/Item.svelte
@@ -0,0 +1,37 @@
+<script>
+  /**
+     * @type {{ name: { str: string; str_sp: string; }; description: string; type:any; damage:any; material: string[] }}
+     */
+  export let data
+  const {damage, name, description, type, material } = data
+</script>
+
+<div class="item-container">
+  <h2> { name.str || name.str_sp || name}</h2>
+  <p>{ description || ''}</p>
+  <p>{ type }</p>
+  {#if damage}
+  <ul>
+    <li>Damage type: {damage.damage_type}</li>
+    <li>Damage: {damage.amount}</li>
+    <li>Armor penetration: {damage.armor_penetration}</li>
+  </ul>
+  {/if}
+  {#if material}
+    <div>
+    Material: [
+    {#each material as item}
+      <span>{item}</span>
+    {/each}
+      ]
+    </div> 
+  {/if}
+</div>
+
+<style>
+  .item-container {
+    border: 2px solid rgb(230,230,230);
+    border-radius: 10px;
+    padding: 2rem;
+  }
+</style>
