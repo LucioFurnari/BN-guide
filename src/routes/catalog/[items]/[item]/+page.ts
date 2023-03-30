c5e810bca68e1faff415143c7475456a37fba6a3
LucioFurnari <luciofurnari99@gmail.com>
2023-03-30 18:03:56 -0300

    Fix end point route of the api url

--- src/routes/catalog/[items]/[item]/+page.ts
+++ src/routes/catalog/[items]/[item]/+page.ts
@@ -1,12 +1,13 @@
 export const load = ({fetch, params}) => {
-  const fetchItem =async (params:string) => {
-    const response = await fetch(`https://raw.githubusercontent.com/cataclysmbnteam/Cataclysm-BN/upload/data/json/items/ammo/${params}`)
+  const {items, item} = params
+  const fetchItem = async (items: string, item: string) => {
+    const response = await fetch(`https://raw.githubusercontent.com/cataclysmbnteam/Cataclysm-BN/upload/data/json/items/${items}/${item}`)
     const data = await response.json()
     return data
   }
 
   return {
-    item: fetchItem(params.item),
+    item: fetchItem(items, item),
     slug: params.item
   }
 }
